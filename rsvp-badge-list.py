3fcecd2d4faadc446ae5ea989672f0642c99d305
Trish Whetzel <plwhetzel@gmail.com>
2016-12-06 00:22:42 -0500

    write badge list to file

--- rsvp-badge-list.py
+++ rsvp-badge-list.py
@@ -46,7 +46,8 @@ def get_upcoming_event_list(my_meetup_api_key_dict):
     meetup_group_name = key_list[0]
     meetup_api_key = my_meetup_api_key_dict[meetup_group_name]
 
-    url = "https://api.meetup.com/"+meetup_group_name+"/events?scroll=recent_past&photo-host=public&page=20&key="+meetup_api_key
+    # Event List doc: http://www.meetup.com/meetup_api/docs/:urlname/events/#list
+    url = "https://api.meetup.com/"+meetup_group_name+"/events?photo-host=public&page=20&key="+meetup_api_key
     r = requests.get(url)
     if r.status_code == 200:
         print('DEBUG: Successful web service call')
@@ -58,7 +59,8 @@ def get_upcoming_event_list(my_meetup_api_key_dict):
         else:
             print("Events found")
             # Get list of event IDs
-            print('Event Name:', str(json_file_contents[0]['name']))
+            event_name = json_file_contents[0]['name'].encode("utf-8")
+            print("Event Name: ", event_name)
 
         # Prompt user for which MeetUp Group API Key to use
         # meetup_group_name = raw_input('Enter the name of your MeetUp group from the list above: ')
@@ -76,6 +78,7 @@ def get_rsvp_badge_list(event_id, my_meetup_api_key_dict):
     key_list = my_meetup_api_key_dict.keys()
     meetup_group_name = key_list[0]
     meetup_api_key = my_meetup_api_key_dict[meetup_group_name]
+    f = open('december-07-2016.txt', 'w')
 
     event_url = "https://api.meetup.com/"+meetup_group_name+"/events/"+event_id+"/rsvps?photo-host=public&sig_id=22749361&key="+meetup_api_key+"&sign=true&fields=answers"
     r = requests.get(event_url)
@@ -83,36 +86,55 @@ def get_rsvp_badge_list(event_id, my_meetup_api_key_dict):
         rsvp_list = r.json()
 
     count = 0
+    name_entered_count = 0
     # Get first and last name of RSVP
     for rsvp in rsvp_list:
         rsvp_status = rsvp['response']
         rsvp_answers = rsvp['answers']
 
+        # Get full name for members who RSVPd Yes
         if rsvp_status == 'yes':
-            full_name = rsvp['member']['name'].strip()
-            if " " in full_name:
-                count += 1
-                # print "Full Name: ", full_name
-                split_name = full_name.split()
-                print(split_name[0], '\t', split_name[1])
+            # Get user profile name
+            user_profile_full_name = rsvp['member']['name'].strip()
+            # print("\nUser Profile Name: "+user_profile_full_name)
 
-            else:
-                # print "Missing full name: ", full_name
-                count += 1
-                # Try to get full name from Answers
-                # print 'Answers:', rsvp_answers
-                for answer in rsvp_answers:
-                    full_name = answer['answer']
-                    if " " in full_name:
-                        # print "Full Name: ", "\"",full_name,"\""
-                        split_name = full_name.split()
-                        print(split_name[0], '\t', split_name[1])
-    # print "Count: ", count
+            # Try to get full name from user entered information in 'Answers'
+            for answer in rsvp_answers:
+                rsvp_full_name = answer['answer']
+                # print("RSVP Full Name: "+"\""+rsvp_full_name+"\"")
+                # Check if user answered question of Full Name
+                if rsvp_full_name != "":
+                    # print("Name entered: "+rsvp_full_name)
+                    count += 1
+                    name_entered_count += 1
+                    # Format name for printing
+                    # TODO: Account for names with multiple spaces and combine appropriately
+                    if " " in rsvp_full_name:
+                        split_name = rsvp_full_name.split()
+                        # print(split_name[0]+'\t'+split_name[1]+"\n")
+                        formatted_rsvp_name = split_name[0]+'\t'+split_name[1]
+                        f.write(formatted_rsvp_name.encode("utf-8")+"\n")
+                else:
+                    # print("Name NOT entered")
+                    count += 1
+                    # print("Profile Name: "+user_profile_full_name)
+                    if " " in user_profile_full_name:
+                        split_name = user_profile_full_name.split()
+                        first_name = split_name.pop(0)
+                        last_name = ""
+                        for name in split_name:
+                            last_name = last_name + name
+                        # print(first_name+'\t'+last_name+"\n")
+                        formatted_name = first_name+'\t'+last_name
+                        f.write(formatted_name.encode("utf-8")+"\n")
+
+    print("Count: ", count)
+    print("Name entered count: ", name_entered_count)
 
 
 # Main #
 if __name__ == '__main__':
     my_meetup_api_key_dict = get_api_key()
     event_id = get_upcoming_event_list(my_meetup_api_key_dict)
-    print("Event ID: ", event_id)
+    # print("Event ID: ", event_id)
     get_rsvp_badge_list(event_id, my_meetup_api_key_dict)
